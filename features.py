ef0541a7c56fa0dca2baff0d5944dd7e82e088f5
Trey Frank Boehm <trey.frank.boehm@gmail.com>
2021-04-27 20:34:00 -0500

    Implement general mobility and material scoring
    The new constant at the top of the file `PieceVectorPositions` tells us
    the position that each piece should take in a vector. I suppose this
    doesn't need to be enforced everywhere, but if you are implementing a
    feature that has some value for all pieces, it will be useful.
    
    I also added docstrings/type hints to any function I touched. While not
    strictly necessary, they're nice to have (and make our project look a
    bit nicer, too).

--- features.py
+++ features.py
@@ -2,34 +2,86 @@
 
 # features.py: Extract features from boards
 
+from collections import OrderedDict
+
 import chess
 import chess.pgn
 
 data_dir = './data/'
 
-PieceCounts = {
-    # White pieces
-    'P': 8,
-    'N': 2,
-    'B': 2,
-    'R': 2,
-    'Q': 1,
-    'K': 1,
-    # Black pieces
-    'p': 8,
-    'n': 2,
-    'b': 2,
-    'r': 2,
-    'q': 1,
-    'k': 1,
+# The initial counts for each piece on the board.
+PieceCounts = OrderedDict(
+    [
+        # White pieces
+        ('K', 1),
+        ('R', 2),
+        ('Q', 1),
+        ('B', 2),
+        ('N', 2),
+        ('P', 8),
+        # Black pieces
+        ('k', 1),
+        ('q', 1),
+        ('r', 2),
+        ('b', 2),
+        ('n', 2),
+        ('p', 8),
+    ]
+)
+
+PieceNames = [piece + str(i) for piece, count in PieceCounts.items() for i in range(count)]
+
+PieceVectorPositions = {piece: i for i, piece in enumerate(PieceNames)}
+
+# This is the most common weight system, but they might not be totally optimal. See this Wikipedia
+# page for more info: https://en.wikipedia.org/wiki/Chess_piece_relative_value
+PieceWeights = {
+    chess.QUEEN: 9,
+    chess.ROOK: 5,
+    chess.BISHOP: 3,
+    chess.KNIGHT: 3,
+    chess.PAWN: 1,
+}
+
+ColorSigns = {
+    chess.WHITE: 1,
+    chess.BLACK: -1,
 }
 
 Pieces = [chess.PAWN, chess.KNIGHT, chess.BISHOP, chess.ROOK, chess.QUEEN, chess.KING]
+
 Colors = [chess.WHITE, chess.BLACK]
 
 
-def get_coordinates(piece, color, piece_name, board):
-    # Coordinates: File, rank, present/not present
+def get_material(board: chess.Board) -> (int, int):
+    """
+    Compute the weighted material for each side.
+
+    :param board: The current board state.
+    :returns: A tuple containing the material scores for black and white.
+    """
+    scores = [0, 0]
+
+    for color in Colors:
+        for piece, weight in PieceWeights.items():
+            piece_count = len(board.pieces(piece, color))
+            scores[color] += piece_count * weight
+
+    return scores
+
+
+def get_coordinates(
+    piece: chess.Piece, color: chess.Color, piece_name: str, board: chess.Board
+) -> list[(int, int, int)]:
+    """
+    Get the rank and file coordinates of all pieces of a given type/color.
+
+    :param piece: The piece type.
+    :param color: The piece color.
+    :param name: The character representing the piece (e.g. 'B' or 'n').
+    :param board: The current board state.
+    :returns: A list of tuples of (rank, file, present_on_board).
+    """
     locs = list(board.pieces(piece, color))
     coords = [(int(loc / 8), int(loc % 8), 1) for loc in locs]
 
@@ -51,7 +103,7 @@ def get_coordinates(piece, color, piece_name, board):
 
 
 class PiecePosition:
-    def __init__(self, game, symbol, rank, file, present):
+    def __init__(self, board: chess.Board, symbol: str, rank: int, file: int, present: int):
         self.symbol = symbol
         self.rank = rank
         self.file = file
@@ -63,7 +115,7 @@ class PiecePosition:
 
 
 class SlidingPiecePosition(PiecePosition):
-    def __init__(self, board, *args):
+    def __init__(self, board: chess.Board, *args):
         super().__init__(board, *args)
         self.legal_moves = board.legal_moves
         self.mobility = dict()  # Depends on the piece
@@ -71,7 +123,7 @@ class SlidingPiecePosition(PiecePosition):
     def __str__(self):
         return f"{self.symbol}: {self.rank}, {self.file}, {self.present}, {self.mobility}"
 
-    def get_mobility(self, offsets):
+    def get_mobility(self, offsets: dict[str, int]):
         mobility = {direction: 0 for direction in offsets.keys()}
 
         for direction, offset in offsets.items():
@@ -139,35 +191,85 @@ PositionClasses = {
 }
 
 
-def board_to_feat(board):
-    feat = dict()
+def get_sliding_mobility(board: chess.Board):
+    """
+    Get the number of spaces sliding pieces can move in each direction.
 
-    # 1: Determine the positions of the pieces that are in play.
+    :param board: The current board state.
+    :returns: TODO (None, currently).
+    """
+    mobilities = dict()
+
     for color in Colors:
         for piece in Pieces:
             # Get the string for each piece
             piece_name = chess.Piece(piece, color).symbol()
             cls = PositionClasses[piece_name.upper()]
-
             coords = get_coordinates(piece, color, piece_name, board)
             for i, position in enumerate(coords):
                 key = piece_name + str(i)
-                feat[key] = position
-                c = cls(board, key, *position)
-                # print(c)
+                # feat[key] = position
+                mobility = cls(board, key, *position)
+                mobilities[key] = mobility
 
-    # Alternative way to determine piece locations
-    # for file in range(8):
-    #    for rank in range(8):
-    #        square = file * 8 + rank
-    #        piece = board.piece_at(square)
-    #        if piece:
-    #            print(f"{piece} is at coordinate ({rank}, {file})")
+
+def get_general_mobility(board: chess.Board) -> list[int]:
+    """
+    Get the number of legal moves for each piece.
+
+    :param board: The current board state.
+    :returns: An list of the number of moves, ordered by PieceVectorPositions.
+    """
+    mobility = [0] * len(PieceNames)
+
+    original_turn = board.turn
+
+    # This looks like a nasty loop nest, but the inner loop only runs 32 times.
+    for color in Colors:
+        board.turn = color
+        moves = set(board.legal_moves)
+        for piece in Pieces:
+            piece_name = chess.Piece(piece, color).symbol()
+            coords = get_coordinates(piece, color, piece_name, board)
+            for i, coordinates in enumerate(coords):
+                key = piece_name + str(i)
+                vector_position = PieceVectorPositions[key]
+                if not coordinates[2]:
+                    # Piece is not in play
+                    move_count = -1
+                else:
+                    # Piece *is* in play. Count how many moves it has.
+                    square = coordinates[0] * 8 + coordinates[1]
+                    move_count = len([move for move in moves if move.from_square == square])
+                mobility[vector_position] = move_count
+
+    return mobility
+
+
+def board_to_feat(board: chess.Board):
+    """
+    Convert a board state into a set of features.
+
+    :param board: The current board state.
+    :returns: A dictionary of features.
+    """
+    feat = dict()
+
+    feat['material'] = get_material(board)
+    feat['mobility'] = get_general_mobility(board)
 
     return feat
 
 
-def get_san_moves(board, moves, move_count=0):
+def get_san_moves(board: chess.Board, moves: list[chess.Move], move_count: int = 0) -> list[str]:
+    """
+    Convert a move list to SAN.
+
+    :param board: The starting board state.
+    :param moves: The list of moves.
+    :param move_count: The maximum number of moves to apply.
+    :returns: A list of moves in standard algebraic notation.
+    """
     san_moves = list()
 
     for move in moves:
@@ -192,5 +294,7 @@ if __name__ == '__main__':
     print(' '.join(san_moves))
 
     feat = board_to_feat(board)
+
+    print("Board features:", feat)
 
     print(board)
